a15e8f30a331611a1f853030c07f9f625eb993d6
Diego Neri <diego@diegoneri.com>
2024-05-03 19:38:38 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿int numero;
+
+Console.Write("Digite um número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero < 0)
+{
+    Console.WriteLine("Negativo");    
+}
+else if (numero > 0)
+{
+    Console.WriteLine("Positivo");
+}
+else
+{
+    Console.WriteLine("Zero");
+}
